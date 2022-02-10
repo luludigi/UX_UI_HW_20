53cbda076b05163cf7b17ff681616080995f9035
LuluDigi <luludigi.design@gmail.com>
2022-02-10 14:56:20 -0600

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,13 +1,9 @@
 // JavaScript Document
 
- <script>
   $( function() {
     $( "#accordion" ).accordion();
   } );
-  </script>
-  
-  <script>
+
   $( function() {
     $( "#tabs" ).tabs();
   } );
-  </script>  
