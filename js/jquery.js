2b744369b42087f87da5dad124c570b728088bd6
LuluDigi <luludigi.design@gmail.com>
2022-02-10 13:22:38 -0600

    js

---
+++ js/jquery.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+ <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+  
+  <script>
+  $( function() {
+    $( "#tabs" ).tabs();
+  } );
+  </script>  
